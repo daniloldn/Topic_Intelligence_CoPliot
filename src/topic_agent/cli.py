a370934fee527c0cba0842ac41d188e7148cca92
daniloldn <daniloldn25@gmail.com>
2026-05-23 11:14:37 +0100

    creating command for the cli to create, update and view all knowledge graphs

--- src/topic_agent/cli.py
+++ src/topic_agent/cli.py
@@ -9,3 +9,11 @@ console = Console()
 def hello(name:str= typer.Argument("World")):
     console.print(f"Hello, [bold green]{name}[/bold green]!")
 
+
+# a command to create a new knowledge graph 
+@app.command()
+def create(topic:str):
+    console.print(f"Starting to create a knowledge history for {topic}")
+
+
+
